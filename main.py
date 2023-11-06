251d2998155ed3ad7446f8ac3d280123773242e1
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2023-11-06 18:48:07 -0300

    Atualizações no main e na func

--- main.py
+++ main.py
@@ -4,22 +4,44 @@ import time
 inScreen = True
 
 def main():
+
+    direcionado = input("O grafo é direcionado? (s/n): ") == 's'
+    valorado = input("O grafo é valorado? (s/n): ") == 's'
+
+    G = criar_grafo(direcionado)
+
     while inScreen:
         limpar_terminal()
         exibir_menu()
-        entrada = input(">> ")
+        
+        opcao = input("Escolha uma opção: ")
         
         try:
-            entrada = int(entrada)
-            if entrada == 1:
-                G = criar_grafo_nao_direcionado()
+            if opcao == '1':
                 adicionar_nos(G)
-                adicionar_arestas(G)
-                visualizar_grafo(G)
-            elif entrada == 0:
+            elif opcao == '2':
+                adicionar_arestas(G, valorado)
+            elif opcao == '3':
+                visualizar_grafo(G, valorado)
+            elif opcao == '4':
+                ordem_e_tamanho_do_grafo(G)
+                time.sleep(2)
+            elif opcao == '5':
+                listar_vertices_adjacentes(G)
+                time.sleep(2)
+            elif opcao == '6':
+                obter_grau_do_vertice(G, direcionado)
+                time.sleep(2)
+            elif opcao == '7':
+                verificar_adjacencia_entre_vertices(G)
+                time.sleep(2)
+            elif opcao == '8':
+                encontrar_caminho_mais_curto(G, valorado)
+                time.sleep(2)
+            elif opcao == '0':
                 exit()
             else:
-                print(f"Valor de entrada {entrada} incorreto, favor digite uma opção válida.")
+                print(f"Valor de entrada {opcao} incorreto, favor digite uma opção válida.")
                 time.sleep(2)
                 
         except ValueError:
--- src/func.py
+++ src/func.py
@@ -4,20 +4,26 @@ import subprocess
 import os
 
 def exibir_menu():
-    print("Menu:")
-    print("1. Opção 1")
-    print("2. Opção 2")
-    print("3. Opção 3")
-    print("4. Opção 4")
-    print("5. Opção 5")
+    print("\nMenu:")
+    print("1. Inserir Nós")
+    print("2. Inserir Arestas")
+    print("3. Visualizar Grafo")
+    print("4. Ordem e Tamanho do Grafo")
+    print("5. Listar Vértices Adjacentes")
+    print("6. Obter Grau do Vértice")
+    print("7. Verificar Adjacência entre Vértices")
+    print("8. Encontrar Caminho Mais Curto")
     print("0. Sair")
 
 def limpar_terminal():
     if subprocess.call("clear" if os.name != 'nt' else 'cls', shell=True) != 0:
         print("Limpeza do terminal não suportada no seu sistema.")
 
-def criar_grafo_nao_direcionado():
-    return nx.Graph()
+def criar_grafo(direcionado):
+    if direcionado:
+        return nx.DiGraph()
+    else:
+        return nx.Graph()
 
 def adicionar_nos(graph):
     while True:
@@ -26,16 +32,77 @@ def adicionar_nos(graph):
             break
         graph.add_node(node)
 
-def adicionar_arestas(graph):
+def adicionar_arestas(graph, valorado):
     while True:
         edge = input("Digite as arestas no formato 'nó1 nó2' (ou pressione Enter para parar): ")
         if not edge:
             break
         node1, node2 = edge.split()
-        graph.add_edge(node1, node2)
+        if valorado:
+            weight = float(input("Digite o peso da aresta: "))
+            graph.add_edge(node1, node2, weight=weight)
+        else:
+            graph.add_edge(node1, node2)
 
-def visualizar_grafo(graph):
+def visualizar_grafo(graph, valorado):
     pos = nx.spring_layout(graph, seed=42)
     nx.draw(graph, pos, with_labels=True, node_size=800, node_color="lightblue", font_size=10, font_color="black", font_weight="bold")
-    plt.title("Grafo Não Direcionado Personalizado")
+    
+    if valorado:
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+    
+    plt.title("Grafo Personalizado")
     plt.show()
+
+def ordem_e_tamanho_do_grafo(graph):
+    ordem = len(graph.nodes)
+    tamanho = len(graph.edges)
+    print(f"Ordem do grafo: {ordem}")
+    print(f"Tamanho do grafo: {tamanho}")
+
+def listar_vertices_adjacentes(graph):
+    node = input("Digite o vértice para listar os vértices adjacentes: ")
+    if node in graph.nodes:
+        adjacentes = list(graph.neighbors(node))
+        print(f"Vértices adjacentes a {node}: {', '.join(adjacentes)}")
+    else:
+        print(f"O vértice {node} não existe no grafo.")
+
+def obter_grau_do_vertice(graph, direcionado):
+    node = input("Digite o vértice para obter o grau: ")
+    if node in graph.nodes:
+        grau = len(list(graph.neighbors(node)))
+        grau_entrada = len(list(graph.predecessors(node))) if direcionado else grau
+        grau_saida = len(list(graph.successors(node))) if direcionado else grau
+        print(f"Grau do vértice {node}: {grau}")
+        if direcionado:
+            print(f"Grau de entrada do vértice {node}: {grau_entrada}")
+            print(f"Grau de saída do vértice {node}: {grau_saida}")
+    else:
+        print(f"O vértice {node} não existe no grafo.")
+
+def verificar_adjacencia_entre_vertices(graph):
+    v1 = input("Digite o primeiro vértice: ")
+    v2 = input("Digite o segundo vértice: ")
+    if graph.has_edge(v1, v2):
+        print(f"{v1} e {v2} são adjacentes.")
+    else:
+        print(f"{v1} e {v2} não são adjacentes.")
+
+def encontrar_caminho_mais_curto(graph, valorado):
+    source = input("Digite o vértice de origem: ")
+    target = input("Digite o vértice de destino: ")
+    if source in graph.nodes and target in graph.nodes:
+        try:
+            if valorado:
+                shortest_path, shortest_distance = nx.single_source_dijkstra(graph, source=source, target=target)
+                print(f"Caminho mais curto de {source} para {target}: {shortest_path}")
+                print(f"Custo do caminho mais curto: {shortest_distance}")
+            else:
+                shortest_path = nx.shortest_path(graph, source=source, target=target)
+                print(f"Caminho mais curto de {source} para {target}: {shortest_path}")
+        except nx.NetworkXNoPath:
+            print(f"Não há caminho entre {source} e {target}.")
+    else:
+        print("Os vértices de origem e/ou destino não existem no grafo.")
