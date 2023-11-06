5af275a97fadfa72b2e40d6cad7f10d046ff0fac
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2023-11-06 16:53:41 -0300

    Update func.py

--- src/func.py
+++ src/func.py
@@ -1,26 +1,26 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Crie um grafo não direcionado
-G = nx.Graph()
+def criar_grafo_nao_direcionado():
+    return nx.Graph()
 
-# Solicite ao usuário para adicionar nós
-while True:
-    node = input("Digite o nome de um nó (ou pressione Enter para parar): ")
-    if not node:
-        break
-    G.add_node(node)
+def adicionar_nos(graph):
+    while True:
+        node = input("Digite o nome de um nó (ou pressione Enter para parar): ")
+        if not node:
+            break
+        graph.add_node(node)
 
-# Solicite ao usuário para adicionar arestas
-while True:
-    edge = input("Digite as arestas no formato 'nó1 nó2' (ou pressione Enter para parar): ")
-    if not edge:
-        break
-    node1, node2 = edge.split()
-    G.add_edge(node1, node2)
+def adicionar_arestas(graph):
+    while True:
+        edge = input("Digite as arestas no formato 'nó1 nó2' (ou pressione Enter para parar): ")
+        if not edge:
+            break
+        node1, node2 = edge.split()
+        graph.add_edge(node1, node2)
 
-# Visualize o grafo
-pos = nx.spring_layout(G, seed=42)
-nx.draw(G, pos, with_labels=True, node_size=800, node_color="lightblue", font_size=10, font_color="black", font_weight="bold")
-plt.title("Grafo Não Direcionado Personalizado")
-plt.show()
+def visualizar_grafo(graph):
+    pos = nx.spring_layout(graph, seed=42)
+    nx.draw(graph, pos, with_labels=True, node_size=800, node_color="lightblue", font_size=10, font_color="black", font_weight="bold")
+    plt.title("Grafo Não Direcionado Personalizado")
+    plt.show()
