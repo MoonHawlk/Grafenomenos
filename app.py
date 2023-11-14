cb20cda2a4b8067e8a5862604b517151a27f3f12
Rafael Lancry <rlc@cesar.school>
2023-11-14 12:46:16 -0300

    Flask Atualizada
    New Branch and New Funcionalities

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('menu.html')
+
+if __name__ == "__main__":
+    app.run()
